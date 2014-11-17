6f2ee9c7f037fec1851b97143265f29d305d91db
Maximiliano Sorribas <mjsorribas@gmail.com>
2014-11-17 14:55:59 -0300

    Se modifico el index para que tome las traducciones

--- app/modules/home/views/page/index.php
+++ app/modules/home/views/page/index.php
@@ -9,12 +9,12 @@ $this->breadcrumbs=array(
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2">
-                <h1 class="header-title">Welcome to</h1>
+                <h1 class="header-title"><?php echo Yii::t('app',"Welcome to");?></h1>
                 <p class="header-text"><?=r()->name?></p>
             </div>
             <div class="col-lg-8 col-lg-offset-2 text-center">
                 <a href="<?=$this->createUrl("/users/page/login")?>" class="btn btn-lg btn-outline">
-                    <i class="fa fa-user"></i> Sign IN
+                    <i class="fa fa-user"></i> <?php echo Yii::t('app',"Sign In");?>
                 </a>
             </div>
         </div>
